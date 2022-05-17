1f1bc3fe29841dd58534d47d64cdfcf1c17b2223
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-05-17 23:47:21 +0700

    return trong ham

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,29 +1,9 @@
-//  Khái niệm hàm (funtion)
-// Tham số trong hàm - Javascript cơ bản
+// Return trong hàm - Javascript cơ bản
 
-/**
- 1. Tham số 
-    - Định nghĩa : là 1 giá trị truyền vào khi gọi tới 1 function
-    - Kiểu dữ liệu? : 
-    - Tính private?
-    - 1 tham số
-    - Nhiều tham số
-
-2. Truyền tham số 
-    - 1 tham số
-    - Nhiều tham số
-    
-3. Arguments 
-    - Đối tượng arguments?
-    - Giới thiệu vòng for
- */
-
-function writeLog() {
-    var myString = '';
-    for (var param of arguments) {
-        myString += `${param} - `
-    }
-    console.log(myString);
+function cong(a, b) {
+    return a + b;
 }
 
-writeLog('LOG 1', 'LOG 2', 'LOG 3', 3, 4, 5);
+var result = cong(2, 8);
+
+console.log(result);
