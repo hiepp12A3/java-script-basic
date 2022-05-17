3f69ce5bf8513981eab863ac31f3f3335d1bc7b5
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-05-17 22:12:34 +0700

    khai niem ham (function)

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,2 +1,25 @@
-var fullName = "Nguyễn Thanh Hiệp";
-alert(fullName);
+//  Khái niệm hàm (funtion)
+
+/**
+ 1. Hàm?
+    - Là một khối mã
+    - Làm 1 việc cụ thể
+
+2. Loại hàm
+    - Built-in
+    - Tự định nghĩa
+
+3. Tính chất
+    - Không thực thi khi định nghĩa
+    - Sẽ thực thi khi được gọi
+    - Có thể nhận tham số
+    - Có thể trả về 1 giá trị
+
+4. Tạo hàm đầu tiên
+ */
+
+function showDialog() {
+    alert('Hi xin chao cac ban');
+}
+
+showDialog();
