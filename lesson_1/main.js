6569f68a3c75e27ac6474bfca6386546a82b6570
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-05-17 23:55:23 +0700

    Hieu hon ve Ham

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,9 +1,37 @@
-// Return trong hàm - Javascript cơ bản
+/**
+ Một số điều cần biết về function
 
-function cong(a, b) {
-    return a + b;
+ 1. Khi function đặt trùng tên? : function định nghĩa sau sẽ ghi đề cái trước
+ 2. Khai báo biến trong hàm? : phạm vi sử dụng của biến trong function thôi
+ 3. Định nghĩa hàm trong hàm?
+ */
+
+
+//  TH1:
+// function showMessage() {
+//     console.log('Message 1');
+// }
+
+// function showMessage() {
+//     console.log('Message 2');
+// }
+// showMessage(); 
+
+
+// TH2:
+// function showMessage() {
+//     var fullName = 'Thanh Hiep';
+//     console.log(fullName);
+// }
+// showMessage();
+
+
+// TH3:
+function showMessage() {
+    function showMessage2() {
+        console.log('Message 2');
+    }
+    showMessage2();
 }
 
-var result = cong(2, 8);
-
-console.log(result);
+showMessage();
