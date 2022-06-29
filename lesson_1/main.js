52ac909fefdf629a36cfcae40fe56152a21c69a2
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-06-29 19:03:54 +0700

    Object co ban

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,16 +1,38 @@
 // Object trong JavaScript
 
-var MyInfo = {
+var emailKey = 'email';
+
+var myInfo = {
     name: 'Hiep Nguyen',
     age: 18,
-    address: 'Ha Noi, VN'
+    address: 'Ha Noi, VN',
+    [emailKey]: 'hiepdeptrai@gmail.com',
+    getName: function() {
+        return this.name;
+    }
 };
 
-// THÊM
-MyInfo.email = 'hiep@gmail.com'; // cách 1 
-MyInfo['my-email'] = 'hiepthanh@gmail.com'; // cách 2: thêm ký tự đặc biệt
+// THÊm
+myInfo.email = 'hiep@gmail.com'; // cách 1 
+myInfo['my-email'] = 'hiepthanh@gmail.com'; // cách 2: thêm ký tự đặc biệt
 
-console.log(MyInfo); // lấy cả Object
-console.log(MyInfo.name); // lấy từng p.tử
-console.log(MyInfo['my-email']); // lấy riêng  p.tử có ký tự đặc biệt
-console.log(MyInfo['job']); // k.có trả về undefined
+// lấy cả Object
+// console.log(myInfo);
+// lấy từng p.tử
+// console.log(myInfo.name);
+// lấy riêng  p.tử có ký tự đặc biệt
+// console.log(myInfo['my-email']);
+// k.có trả về undefined
+// console.log(myInfo['job']);
+
+// XÓA
+delete myInfo.age;
+delete myInfo.address;
+console.log(myInfo);
+
+//  gọi function trong object
+console.log(myInfo.getName());
+
+// CÁCH GỌI
+// Function --> Phương thức / method
+// oTHERS --> Thuộc tính / Property
