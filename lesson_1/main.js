3158be0f10490619e27a86eeeb1a0994834cfeed
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-07-01 00:04:57 +0700

    lenh ra nhanh if-else

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,48 +1,15 @@
 /**
- *  MATH OBJECT
-  
-    - Math.PI
-    - Math.round()
-    - Math.abs()
-    - Math.ceil()
-    - Math.floor()
-    - Math.random()
-    - Math.min()
-    - Math.max()
+ * Câu lệnh rẽ nhánh - If else
  */
 
-// trả về số PI 3.141592653589793
-console.log(Math.PI);
+var date = 83248;
 
-// làm tròn số
-console.log(Math.round(1.3));
-
-// Nhân về giá trị tuyệt đối 
-// biến số âm thành số dương
-console.log(Math.abs(4));
-
-// chỉ làm tròn trên (phải lớn hơn 0) ví dụ 4.1 =5, 4.6 =5;
-console.log(Math.ceil(5.1));
-
-// làm tròn dưới (ngược với ceil())
-console.log(Math.floor(4.9))
-
-// trả về ngẫu nhiên nhỏ hơn 1
-console.log(Math.random())
-    // VD tạo số ngẫu nhiên 
-console.log(Math.floor(Math.random() * 1000))
-    // vd: tạo random ngẫu nhiên
-var random = Math.floor(Math.random() * 4);
-var bonus = [
-    '10 coin',
-    '20 coin',
-    '30 coin',
-    '40 coin',
-];
-console.log(bonus[random]);
-
-// lấy ra số nhỏ nhất
-console.log(Math.min(-100, 3, 4, 42, 324))
-
-// lấy ra số lớn nhất
-console.log(Math.max(-100, 3, 4, 42, 324))
+if (date === 2) {
+    console.log('Hom nay la thu 2');
+} else if (date === 3) {
+    console.log('Hom nay la thu 3');
+} else if (date === 4) {
+    console.log('Hom nay la thu 4');
+} else {
+    console.log('khong biet')
+}
