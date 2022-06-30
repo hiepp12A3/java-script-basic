25085f8a7481c0714ea1297aedd05c43cba366bf
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-06-30 23:34:10 +0700

    Math Object

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,25 +1,48 @@
 /**
- *  ĐỐI TƯỢNG DATE
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+ *  MATH OBJECT
+  
+    - Math.PI
+    - Math.round()
+    - Math.abs()
+    - Math.ceil()
+    - Math.floor()
+    - Math.random()
+    - Math.min()
+    - Math.max()
  */
 
+// trả về số PI 3.141592653589793
+console.log(Math.PI);
 
+// làm tròn số
+console.log(Math.round(1.3));
 
-var date = new Date();
+// Nhân về giá trị tuyệt đối 
+// biến số âm thành số dương
+console.log(Math.abs(4));
 
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-var day = date.getDate();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
+// chỉ làm tròn trên (phải lớn hơn 0) ví dụ 4.1 =5, 4.6 =5;
+console.log(Math.ceil(5.1));
 
-console.log(`${day}/${month}/${year}`)
+// làm tròn dưới (ngược với ceil())
+console.log(Math.floor(4.9))
 
-console.log(date)
-console.log(day)
-console.log(year)
-console.log(month)
-console.log(hours)
-console.log(minutes)
-console.log(seconds)
+// trả về ngẫu nhiên nhỏ hơn 1
+console.log(Math.random())
+    // VD tạo số ngẫu nhiên 
+console.log(Math.floor(Math.random() * 1000))
+    // vd: tạo random ngẫu nhiên
+var random = Math.floor(Math.random() * 4);
+var bonus = [
+    '10 coin',
+    '20 coin',
+    '30 coin',
+    '40 coin',
+];
+console.log(bonus[random]);
+
+// lấy ra số nhỏ nhất
+console.log(Math.min(-100, 3, 4, 42, 324))
+
+// lấy ra số lớn nhất
+console.log(Math.max(-100, 3, 4, 42, 324))
