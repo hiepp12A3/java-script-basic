6f8d1c0eca5a76239e2b1ebae905a0d2548975c1
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-06-30 10:22:58 +0700

    Date()

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,36 +1,25 @@
 /**
-    Object Prototype
-
-    1.Prototype là gì?
-    2. Sử dụng khi nào?
-    Thêm được phương thức và thuộc tính bên ngoài hàm tạo
-    
+ *  ĐỐI TƯỢNG DATE
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
  */
 
 
-var User = function User(firstName, lastName, avatar) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.avatar = avatar;
 
-    this.getName = function() {
-        return `${this.firstName} & ${this.lastName}`;
-    }
-}
+var date = new Date();
 
-// Thêm thuộc tính bên ngoài hàm tạo
-User.prototype.className = 'F8';
-// thêm phương thức bằng Prototype
-User.prototype.getClassName = function() {
-    return this.className;
-}
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
 
-var author = new User('Hiep', 'Thanh', 'dep trai sieu cap');
-var user = new User('Son', 'Ngong', 'dep trai sieu cap');
+console.log(`${day}/${month}/${year}`)
 
-console.log(author);
-console.log(user);
-
-//  gọi thuộc tính tạo bằng prototype
-console.log(author.className);
-console.log(user.getClassName());
+console.log(date)
+console.log(day)
+console.log(year)
+console.log(month)
+console.log(hours)
+console.log(minutes)
+console.log(seconds)
