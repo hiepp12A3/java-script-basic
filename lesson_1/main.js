87b7913aed12bccd06239e33aa71c78fc3d1435d
Nguyễn Thanh Hiệp <hiepnguyen31899@gmail.com>
2022-07-01 14:41:27 +0700

    them vidu cho vong lap for()

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -10,6 +10,28 @@
 
 
 //  VÒNG LẶP FOR
-for (var i = 1; i <= 10; i++) {
-    console.log(i);
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+/**
+    Bài tập trên F8:
+    - Tạo hàm getRandNumbers , có 3 tham só min, max, length.
+    - Hàm trả về số 1 mảng gồm length phần tử.
+    - Các giá trị trong mảng là số ngẫu nhiên, giá trị trong khoảng min tới max.
+    Gợi ý: cách taoh số 1 số ngẫu nhiên trong khoảng min, max
+    Math.random() * (max - min) + min
+    M
+
+ */
+
+const getRandNumbers = (min, max, length) => {
+    const array = []
+    min = 5;
+    max = 16;
+    for (let i = 0; i < length; i++) {
+        array.push(Math.floor(Math.random() * (max - min + 1) + min))
+    }
+    return array
 }
+console.log(getRandNumbers(1, 9, 5))
